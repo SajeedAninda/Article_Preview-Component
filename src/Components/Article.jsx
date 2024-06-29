acd5d10825445f193b9e2c4a9c04a478a40432e5
SajeedAninda <sajaninda@gmail.com>
2024-06-29 17:04:20 +0600

    Made responsive for mobile version

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -14,7 +14,6 @@ const Article = () => {
     }
 
 
-
     return (
         <div className='w-full h-fit  md:h-screen py-8 md:py-0 bg-[#ecf2f8] flex justify-center items-center relative'>
             <div className='h-fit w-[90%] md:w-[60%] rounded-2xl bg-white flex flex-col md:flex-row  overflow-hidden'>
@@ -54,7 +53,7 @@ const Article = () => {
                         </div>
 
                         {showTooltip &&
-                            <div className='absolute bottom-72 right-52 w-56 px-8 py-4 z-10 bg-[#48556a] text-white text-center rounded-lg shadow-lg flex justify-between items-center'>
+                            <div className='hidden md:flex absolute bottom-72 right-52 w-56 px-8 py-4 z-10 bg-[#48556a] text-white text-center rounded-lg shadow-lg justify-between items-center'>
                                 <p className='text-[13px] text-[#a9bddf] font-bold tracking-widest'>SHARE</p>
                                 <div className='flex space-x-2'>
                                     <img className='w-[18px] cursor-pointer' src={fbIcon} alt="Facebook Icon" />
@@ -62,6 +61,22 @@ const Article = () => {
                                     <img className='w-[18px] cursor-pointer' src={pinIcon} alt="Pinterest Icon" />
                                 </div>
                                 <div className='absolute bottom-[-6px] left-1/2 transform -translate-x-1/2 w-3 h-3 bg-[#48556a] rotate-45'></div>
+                            </div>
+                        }
+
+                        {showTooltip &&
+                            <div className='flex md:hidden absolute w-[79%] px-8 py-4 z-50 bg-[#48556a] text-white text-center rounded-lg shadow-lg justify-between items-center'>
+                                <p className='text-[13px] text-[#a9bddf] font-bold tracking-widest'>SHARE</p>
+                                <div className='flex space-x-2'>
+                                    <img className='w-[18px] cursor-pointer' src={fbIcon} alt="Facebook Icon" />
+                                    <img className='w-[18px] cursor-pointer' src={twitterIcon} alt="Twitter Icon" />
+                                    <img className='w-[18px] cursor-pointer' src={pinIcon} alt="Pinterest Icon" />
+                                </div>
+                                <div onClick={toggleTooltip} className='flex justify-end items-center cursor-pointer'>
+                            <div className='p-2 bg-[#e8ebf0] rounded-full'>
+                                <img className='w-[17px]' src={shareIcon} alt="Share Icon" />
+                            </div>
+                        </div>
                             </div>
                         }
                     </div>
